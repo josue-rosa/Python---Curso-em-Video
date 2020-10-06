0bf5b5992882a0c208ddac3314cf0aeef9f249db
josue-rosa <josuecamponogara@hotmail.com>
2020-10-06 18:24:01 -0300

    .

--- Aula 7 - Operadores Aritméticos/aula07a.py
+++ Aula 7 - Operadores Aritméticos/aula07a.py
@@ -5,9 +5,10 @@
 
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
-print(f'A soma vale {n1+n2}')
-print(f'A multiplicação é {n1*n2}')
+
+print(f'A soma vale {n1+n2} e', end=" >>> ")  # "end=" " tirar a quebra de linha e colocar na mesma linha
+print(f'a multiplicação é {n1*n2}.')
 print(f'A potenciação é {n1**n2}')
-print(f'A divisao é {n1/n2:.2f}')
+print(f'A divisao é {n1/n2:.2f}')  # ":.2f" e para colocar duas casas decimais
 print(f'A divisao inteira é {n1//n2}')
 print(f'O resto da divisao é {n1%n2}')
---
+++ Aula 7 - Operadores Aritméticos/desafios.py
@@ -0,0 +1,4 @@
+# ler um numero inteiro e mostre o antecessor e sucessor
+numero = int(input('Informe um numero inteiro: '))
+print(f'O numero digitado foi {numero}. \nO seu antecessor é {numero -1} e ', end="")
+print(f'o seu sucessor é o {numero + 1}')
