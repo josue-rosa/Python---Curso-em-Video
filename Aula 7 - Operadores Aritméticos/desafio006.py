590ec07609a4f86afd3cf8946878cf5712f36c6b
josue-rosa <josuecamponogara@hotmail.com>
2020-10-07 08:42:29 -0300

    .

---
+++ Aula 7 - Operadores Aritméticos/desafio006.py
@@ -0,0 +1,6 @@
+# ler um numero e mostre o seu dobro, triplo e raiz quadrada
+n1 = int(input('Digite um numero: '))
+
+print(f'Dobro: {n1*2}')
+print(f'Triplo: {n1*3}')
+print(f'Raiz Quadrada: {n1**(1/2)}')
