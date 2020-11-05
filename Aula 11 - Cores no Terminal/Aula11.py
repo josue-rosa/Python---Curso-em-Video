10c4f0faeef2e7b6af7f3898f94ba84f031a89a4
josue-rosa <josuecamponogara@hotmail.com>
2020-11-05 10:25:33 -0300

    update

--- Aula 11 - Cores no Terminal/Aula11.py
+++ Aula 11 - Cores no Terminal/Aula11.py
@@ -7,7 +7,7 @@ print('\033[4;30;45mOlá, mundo')
 cores = {'limpa': '\033[m',
          'azul': '\033[36m',
          'amarelo': '\033[33m',
-         'pretoebranco': '\033[7;30m'}
+         'preto_e_branco': '\033[7;30m'}
 
 nome = 'Josué'
 print(f'Prazer em te conhecer {cores["azul"]}{nome}')
