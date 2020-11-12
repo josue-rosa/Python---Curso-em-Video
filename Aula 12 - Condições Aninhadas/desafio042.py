34760d50b4bcdfd0f89e529e9564d3b882c73afd
josue-rosa <josuecamponogara@hotmail.com>
2020-11-12 18:02:15 -0300

    Update desafio042.py

--- Aula 12 - Condições Aninhadas/desafio042.py
+++ Aula 12 - Condições Aninhadas/desafio042.py
@@ -7,6 +7,18 @@
 - acima de 40: Obesidade Mórbida
 """
 
-peso = float(input('Informe o seu peso'))
-altura = float(input('Informe a sua altura'))
+peso = float(input('Informe o seu peso em Kg: '))
+altura = float(input('Informe a sua altura em M: '))
 
+imc = peso / (altura * altura)
+
+if imc < 18.5:
+    print('Abaixo do peso')
+elif imc >= 18.5 and imc < 25:
+    print('Peso Ideal')
+elif imc >= 25 and imc < 30:
+    print('Sobrepeso')
+elif imc >= 30 and imc < 40:
+    print('Obesidade')
+else:
+    print('Obesidade mórbida')
