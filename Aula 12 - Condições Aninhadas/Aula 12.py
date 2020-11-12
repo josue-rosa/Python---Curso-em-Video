d89799b2dc396dddd113f74e87fd528e9416b38f
josue-rosa <josuecamponogara@hotmail.com>
2020-11-11 23:45:36 -0300

    aula

---
+++ Aula 12 - Condições Aninhadas/Aula 12.py
@@ -0,0 +1,7 @@
+nome = str(input('Informe seu nome: ')).title()
+if nome == 'Josué' or nome == 'Davi':
+    print(f'Que nome lindo! Bem-vindo {nome}!')
+elif nome == 'Paloma':
+    print('Você é a pessoa que cuida do Davi. Bem-vinda')
+else:
+    print(f'Prazer em te conhecer {nome}')
