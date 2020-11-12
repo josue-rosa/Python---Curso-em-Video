b35406719f1e4ec2f35ecf28d845304e70124212
josue-rosa <josuecamponogara@hotmail.com>
2020-11-12 15:31:17 -0300

    update

--- Aula 12 - Condições Aninhadas/desafio039.py
+++ Aula 12 - Condições Aninhadas/desafio039.py
@@ -1,5 +1,5 @@
 """
-Programa que leia o ano de nascimento de um jovem e informem de acordo com sua idade:
+Programa que leia o ano de nascimento de um jovem e informe de acordo com sua idade:
 
 - Se ele ainda vai se alistar ao serviço militar
 - se é a hora de ele se alistar
@@ -7,3 +7,19 @@ Programa que leia o ano de nascimento de um jovem e informem de acordo com sua i
 
 seu programa também devera mostrar o tempo que falta ou que passou do prazo
 """
+from datetime import date
+
+
+ano_jovem = int(input('Informe seu ano de nascimento: '))
+ano = date.today().year - ano_jovem
+if ano < 18:
+    print(f'Você tem {ano} anos')
+    print('Você ainda vai se alistar')
+    print(f'Faltam {18 - ano} para o alistamento')
+elif ano == 18:
+    print(f'Você tem {ano} anos')
+    print('é hora de você se alistar')
+else:
+    print(f'Você tem {ano} anos')
+    print(f'Já passou do tempo de se alistar. \nVocê Você deveria ter se alistado há {ano - 18} anos')
+    
