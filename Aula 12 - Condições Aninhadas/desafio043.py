69931036145f7572197d972e4e96c8124ec068b9
josue-rosa <josuecamponogara@hotmail.com>
2020-11-13 10:37:48 -0300

    Update desafio043.py

---
+++ Aula 12 - Condições Aninhadas/desafio043.py
@@ -0,0 +1,8 @@
+"""Programa que calcule o valor a ser pago por um produto, considerando o seu Preço normal e condição de pagamento
+
+- a vista dinheiro/cheque: 10% de desconto
+- a vista cartao: 5% de desconto
+- em até 2x no cartao: preço normal
+- em 3x ou mais no cartão: 20% juros
+"""
+ 
