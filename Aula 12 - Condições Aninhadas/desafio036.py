6e84dde4ccf91bc67231096f14c131534e229cf6
josue-rosa <josuecamponogara@hotmail.com>
2020-11-13 15:04:16 -0300

    Update desafio036.py

--- Aula 12 - Condições Aninhadas/desafio036.py
+++ Aula 12 - Condições Aninhadas/desafio036.py
@@ -4,12 +4,13 @@ o programa vai perguntar o valor da casa, o salario do comprador e em quantos an
 calcule o valor prestação mensal sabendo que ele não pode exceder 30% do salario ou então o emprestimo será negado
 """
 
-valor_casa = float(input('Qual o valor da casa, em R$: '))
-salario_comprador = float(input('Qual o salário do comprador? '))
+valor_casa = float(input('Qual o valor da casa? R$ '))
+salario_comprador = float(input('Qual o salário do comprador? R$ '))
 qtd_anos = int(input('Quantidade de anos para pagar? '))
 
-qtd = qtd_anos * 12 # conversão de tempo em ANOS para MÊS
-calculo_prestacao = (valor_casa / qtd)
+calculo_prestacao = valor_casa / (qtd_anos * 12) # conversão de tempo em ANOS para MÊS
+
+# Calculo de 30% do salario
 calculo_30salario = (salario_comprador * 0.30) 
 
 print(f'Para pagar uma casa de R$ {valor_casa:.2f} em {qtd_anos} anos, a prestação será de R$ {calculo_prestacao:.2f}')
