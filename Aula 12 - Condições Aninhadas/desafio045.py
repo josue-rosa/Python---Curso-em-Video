910220f28d356521e0af7a845118598b0f5208f0
josue-rosa <josuecamponogara@hotmail.com>
2020-11-15 18:52:13 -0300

    Update desafio045.py

--- Aula 12 - Condições Aninhadas/desafio045.py
+++ Aula 12 - Condições Aninhadas/desafio045.py
@@ -1,2 +1,47 @@
 """programa que faça o computador jogar jokenpô com você"""
 
+from time import sleep
+from random import randint
+
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+
+jogador = int(input('''Qual é a sua jogada?
+[ 0 ] - PEDRA
+[ 1 ] - PAPEL
+[ 2 ] - TESOURA
+'''))
+print('-=' * 15)
+print(f'O computador jogou: {itens[computador]}')
+print(f'O jogador jogou {itens[jogador]}')
+print('-=' *15)
+
+if computador == 0: # computador jogou PEDRA
+    if jogador == 0:
+        print('EMPATOU')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCEU')
+    else:
+        print('JOGADA INVÁLIDA') 
+
+elif computador == 1: # COMPUTADOR jogou PAPEL
+    if jogador == 1:
+        print('EMPATOU')
+    elif jogador == 0:
+        print()
+    elif jogador == 2:
+        print('')
+    else:
+        print('JOGADA INVÁLIDA')
+
+elif computador == 2: # COMPUTADOR JOGOU TESOURA
+    if jogador == 2:
+        print('')
+    elif jogador == 0:
+        print('')
+    elif jogador == 1:
+        print('')
+    else:
+        print('JOGADA INVÁLIDA')
