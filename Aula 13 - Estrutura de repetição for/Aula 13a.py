9f86f3ec87a307c2a78acdbba0169546070fa56b
josue-rosa <josuecamponogara@hotmail.com>
2020-11-15 20:12:39 -0300

    update

--- Aula 13 - Estrutura de repetição for/Aula 13a.py
+++ Aula 13 - Estrutura de repetição for/Aula 13a.py
@@ -1,11 +1,37 @@
+
+
+soma = 0
+for c in range(0, 3):
+    n = int(input('Digite um valor: '))
+    soma += n
+print(f'O somatório dos valores foi: {soma}')
+
+
+# Exemplo 3
+"""
+inicio = int(input('inicio: '))
+fim = int(input('fim: '))
+passo = int(input('passo: '))
+
+for c in range(inicio, fim+1, passo):
+    print(c)
+print('FIM')
+"""
+
+
+# Exemplo 2
+"""
 n = int(input('Digite um numero '))
 for c in range(1, n+1, 1):
     print(c)
 print('FIM')
+"""
 
+
+# Exemplo 1
 """
 for c in range(7, 0, -1):
     # print('oi')
     print(c)
-print('fim')
+print('FIM')
 """
