2e1a741366dfc5d789aa4e2b3f5dccb022f1209c
josue-rosa <josuecamponogara@hotmail.com>
2020-11-17 10:52:02 -0300

    update

--- Aula 13 - Estrutura de repetição for/desafio051.py
+++ Aula 13 - Estrutura de repetição for/desafio051.py
@@ -1,5 +1,9 @@
 """Programa que leia o primeiro termo e a razão de uma PROGRESSÃO
 ARITMÉTICA. No final, mostre os 10 primeiros termos dessa progressão."""
 
-for c in range(1, 11):
-    print(f'Primeiro termo {c} e razão {c[2] - c[1]}')
+primeiro_termo = int(input('Informe o primeiro termo: '))
+razao = int(input('Informe a razão: '))
+decimo = primeiro_termo + (10 - 1) * razao  # calcular enésimo
+for c in range(primeiro_termo, decimo + razao, razao):
+    print(c, end=' -> ')
+print('FINISH')
--- Aula 13 - Estrutura de repetição for/desafio052.py
+++ Aula 13 - Estrutura de repetição for/desafio052.py
@@ -1,3 +1,20 @@
 """Programa que leia um numero inteiro e diga se ele é ou não
 um número primo"""
-
+total = 0
+print('''
+Pra ser PRIMO, um número tem que ser divisível por 1 e por ELE MESMO. 
+Portanto, ele será divisivel 2 vezes.
+''')
+numero = int(input('Digite um numero: '))
+for c in range(1, numero + 1):
+    if numero % c == 0:
+        print('\033[34m', end=' ')
+        total += 1
+    else:
+        print('\033[31m', end=' ')
+    print(f'{c}', end=' ')
+print(f'\n\033[mO número {numero} foi divisivel {total} vezes')
+if total == 2:
+    print('portanto, ELE É PRIMO')
+else:
+    print('Portanto, NÃO É PRIMO')
