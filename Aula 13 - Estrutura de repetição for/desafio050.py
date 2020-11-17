fb7560820180e5e12ca53fadb9000f2b60a66179
josue-rosa <josuecamponogara@hotmail.com>
2020-11-16 23:19:21 -0300

    update

--- Aula 13 - Estrutura de repetição for/desafio050.py
+++ Aula 13 - Estrutura de repetição for/desafio050.py
@@ -3,8 +3,10 @@ programa que leia 6 numero inteiros e mostre a soma apenas daqueles
 que forem pares. Se o valor digitado for impar, desconsidere-o.
 """
 soma = 0
+cont = 0
 for c in range(1, 7):
-    num = int(input('Digite um número: '))
+    num = int(input(f'Digite o {c} valor: '))
     if num % 2 == 0:
+        cont += 1
         soma += num
-print(soma)
+print(f'Você informou {cont} números PARES e a soma foi {soma}')
