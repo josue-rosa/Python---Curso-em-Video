288d9dd663c159def16ae8a6ef3f96c6d73b1544
josue-rosa <josuecamponogara@hotmail.com>
2020-12-02 17:23:35 -0300

    update

--- Aula 08 - Utilizando Módulos/aula08a.py
+++ Aula 08 - Utilizando Módulos/aula08a.py
@@ -8,7 +8,7 @@ print(f"Arredondando para cima: {math.ceil(raiz):.2f}")
 print(f"Arredondando para baixo: {math.floor(raiz):.2f}")
 """
 
-# Também é possível importar diretamente os múdulos sqrt, ceil e floor
+# Também é possível importar diretamente os módulos sqrt, ceil e floor
 """from math import sqrt, ceil, floor
 num = int(input('Digite um numero: '))
 raiz = sqrt(num)
@@ -18,15 +18,17 @@ print(f'Arredondando para baixo: {floor(raiz):.2f}')
 """
 
 import random
-""" Exibir um valor ramdomico entre 0 e 1
+""" Exibir um valor randomico entre 0 e 1
 num = random.random()
 print(num)
 """
 
-""" Exibir um valor ramdomico INTEIRO entre 1 e 10
+""" Exibir um valor randomico INTEIRO entre 1 e 10
 num = random.randint(1, 10)
 print(num)
 """
 
 import emoji
 print(emoji.emojize("Olá Mundo, :sunglasses:", use_aliases=True))
+
+# https://www.webfx.com/tools/emoji-cheat-sheet/
--- Aula 13 - Estrutura de repetição for/desafio046-Fogos de artificio.py
+++ Aula 13 - Estrutura de repetição for/desafio046-Fogos de artificio.py
@@ -2,8 +2,9 @@
 de fogos de artificio, indo de 10 até 0, com uma pausa de 1 segundo entre eles"""
 
 from time import sleep
+import emoji
 
 for c in range(10, -1, -1):
     print(c)
-    sleep(0.5)
-print('BOOM')
+    sleep(1)
+print(emoji.emojize(':fire: :boom: :sparkles: :fireworks:', use_aliases=True))
