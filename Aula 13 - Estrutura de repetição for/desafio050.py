3701a42ffbaf5a27eda81c9a52aaf65dba363719
josue-rosa <josuecamponogara@hotmail.com>
2020-12-04 09:29:01 -0300

    .

--- Aula 13 - Estrutura de repetição for/desafio050.py
+++ Aula 13 - Estrutura de repetição for/desafio050.py
@@ -1,11 +1,11 @@
 """
-programa que leia 6 numero inteiros e mostre a soma apenas daqueles
+programa que leia 6 números inteiros e mostre a soma apenas daqueles
 que forem pares. Se o valor digitado for impar, desconsidere-o.
 """
 soma = 0
 cont = 0
 for c in range(1, 7):
-    num = int(input(f'Digite o {c} valor: '))
+    num = int(input(f'Digite o {c}° valor: '))
     if num % 2 == 0:
         cont += 1
         soma += num
--- Aula 13 - Estrutura de repetição for/desafio053.py
+++ Aula 13 - Estrutura de repetição for/desafio053.py
@@ -3,14 +3,4 @@ desconsiderando os espaços"""
 
 # Exemplo: Apos a Sopa, A sacada da casa, a torre da derrota
 
-frase = str(input('digite uma frase: ')).lower().strip()
-spliter = frase.split()
-junto = ''.join(spliter)
-frase_inverso = junto[::-1]
-
-print(f'O inverso de {junto} é {frase_inverso}')
-
-if frase_inverso == junto:
-    print('Esta frase é um palíndromo')
-else:
-    print('Não é palindromo')
+frase = str(input('Digite uma frase ')).strip().lower()
