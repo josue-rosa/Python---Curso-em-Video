7f0a4db314be9561d104c7aabeebb90d1beb9486
josue-rosa <josuecamponogara@hotmail.com>
2020-12-18 16:03:20 -0300

    .

--- Aula 15 - Interrompendo repetições while/desafio069.py
+++ Aula 15 - Interrompendo repetições while/desafio069.py
@@ -1,4 +1,34 @@
+tot18 = totalH = totalM_20 = 0
 
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F] ')).upper().strip()[0]
+
+    if idade > 18:
+        tot18 += 1
+
+    if sexo == 'M':
+        totalH += 1
+
+    if sexo == 'F' and idade < 20:
+        totalM_20 += 1
+
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    if resp == 'N':
+        break
+
+print(f'Total de pessoas com mais de 18 anos: {tot18}.')
+print(f'Ao todo temos {totalH} homens cadastrados.')
+print(f'E temos {totalM_20} mulheres com menos de 20 anos.')
+
+
+# Meu código
+
+"""
 resp = 'S'
 idade = int(input('Informe a idade: '))
 sexo = str(input('Informe o sexo [M/F]: ')).upper().strip()[0]
@@ -11,6 +41,7 @@ while resp == 'S':
         sexo_masc += 1
     elif sexo in 'Ff' and idade < 20:
         total_mulher_menor_20 += 1
+
     resp = str(input('Quer continuar [S/N]: ')).upper().strip()[0]
     if resp in 'Ss':
         idade = int(input('Informe a idade: '))
@@ -19,3 +50,4 @@ while resp == 'S':
 print(f'Foram encontradas {maior18} Pessoas acima de 18 anos.')
 print(f'{sexo_masc} homens foram cadastrados')
 print(f'{total_mulher_menor_20} mulheres tem menos de 20 anos.')
+"""
--- Aula 15 - Interrompendo repetições while/desafio070.py
+++ Aula 15 - Interrompendo repetições while/desafio070.py
@@ -1,4 +1,34 @@
+total = totmil = cont = menor = 0
+barato = ''
+while True:
+    produto = str(input('Nome do produto: '))
+    preco = float(input('Preço: R$ '))
+    cont += 1
+    total += preco
 
+    if preco > 1000:
+        totmil += 1
+
+    if cont == 1 or preco < menor:
+        barato = produto
+        menor = preco
+
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar: [S/N] ')).upper().strip()[0]
+    if resp == 'N':
+        break
+
+print('{:-^40}'.format(" Fim do programa "))
+print(f'O total da compra foi R$ {total:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1.000 ')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
+
+
+# Meu código
+# Faltou fazer o nome do produto mais barato
+
+"""
 nome_produto = str(input('Nome do produto: ')).strip()
 preco_produto = float(input('Preço: '))
 resp = 'S'
@@ -20,3 +50,4 @@ while True:
 
 print(f'Gasto total em compras: R${valor_total_produtos:.2f}')
 print(f'{cont_preco} produtos custam acima de MIL Reais')
+"""
